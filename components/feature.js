ffdb22f064c2094155a6ce411b21999e240c1622
Ippei Sumida <ippey.s@gmail.com>
2021-07-31 15:45:49 +0900

    一旦作成

---
+++ components/feature.js
@@ -0,0 +1,69 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
+
+const features = [
+    {
+        name: 'プログラミングワークショップ',
+        description:
+            '初めての子どもからベテランまで参加できる、無料プログラミングワークショップを運営・開催しています。',
+        icon: GlobeAltIcon,
+    },
+    {
+        name: 'メンター',
+        description:
+            'ワークショップ内で、子どもたちのプログラミングのサポートを行っています。',
+        icon: ScaleIcon,
+    },
+    {
+        name: '特別ワークショップ',
+        description:
+            '外部イベントなどで、無料プログラミングワークショップを開催します。',
+        icon: LightningBoltIcon,
+    },
+    {
+        name: '図書貸出',
+        description:
+            'CoderDojo枚方で所有している書籍を子どもたちに無料で貸し出ししています。',
+        icon: AnnotationIcon,
+    },
+]
+
+export default function Feature() {
+    return (
+        <div className="py-12 bg-white mt-10">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="lg:text-center">
+                    <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">About</h2>
+                    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                        子どもたちに自由なプログラミング体験を
+                    </p>
+                    <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                        CoderDojo枚方は、子どもたちのための、オープンソース・プログラミング道場（クラブ、ワークショップ）で、子どもたちが無料で主体的にプログラミングを学習・体験できます。全世界に2,000ヶ所以上の道場があり、枚方は1,000番目の道場です。
+                    </p>
+                    <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                        ワークショップは5歳から17歳までの子どもが参加することができ、『メンター』と呼ばれるサポート役が、子どもたちのプログラミングのお手伝いをします。
+                    </p>
+                    <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                        CoderDojo枚方では主に以下の活動を行っています。
+                    </p>
+                </div>
+
+                <div className="mt-10">
+                    <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                        {features.map((feature) => (
+                            <div key={feature.name} className="relative">
+                                <dt>
+                                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                        <feature.icon className="h-6 w-6" aria-hidden="true" />
+                                    </div>
+                                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                                </dt>
+                                <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+                            </div>
+                        ))}
+                    </dl>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,47 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Hero from "../components/hero";
+import Feature from "../components/feature";
+import {zenUrl} from "../components/navigation";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div >
+        <Hero />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Feature />
+        <a id="about" />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <a id="mentor" />
+        <div className="py-12 bg-white mt-10">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="lg:text-center">
+                    <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">メンター募集</h2>
+                    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                        子どもたちに自由なプログラミング体験を
+                    </p>
+                    <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                        CoderDojo枚方は、子どもたちのプログラミングをサポートするメンターを募集しています。
+                    </p>
+                    <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                        希望の方は、<a href={ zenUrl } target="_blank">こちら</a>よりCoderDojo枚方のメンバーに登録いただき、DMよりご連絡ください。なお、メンターでご協力いただくにあたり、事前にお打ち合わせをさせていただきます。
+                    </p>
+                    <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                        <div className="rounded-md shadow">
+                            <a
+                                href={zenUrl}
+                                target="_blank"
+                                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                            >
+                                メンター参加する
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </main>
 
       <footer className={styles.footer}>
         <a
