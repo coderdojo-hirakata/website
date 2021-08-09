c56e92561912a45577d2bd1d5f8cc382b7950fcd
Ippei Sumida <ippey.s@gmail.com>
2021-08-09 10:05:17 +0900

    別窓に変更

--- components/eventBanner.js
+++ components/eventBanner.js
@@ -23,6 +23,7 @@ class EventBanner extends Component {
                         <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
                             <a
                                 href={ zenUrl }
+                                target="_blank"
                                 className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-primary bg-white"
                             >
                                 参加する
